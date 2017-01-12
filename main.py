bbea939d33ef78f5170a63b50c8477cdaed5bc8d
Alex Tomlins <alex@tomlins.org.uk>
2017-01-12 20:50:01 +0000

    Add main routine to connect to Wi-Fi and start reading temps

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import config
+import network
+import readtemps
+
+def wifi_connect(ssid, password):
+    sta_if = network.WLAN(network.STA_IF)
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(ssid, password)
+        while not sta_if.isconnected():
+            pass
+    print('network config:', sta_if.ifconfig())
+
+
+wifi_connect(config.WIFI_NAME, config.WIFI_PASSWORD)
+
+readtemps.run()
