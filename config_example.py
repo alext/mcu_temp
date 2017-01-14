03fbf5280e02dac75286d7d1d360468990abc407
Alex Tomlins <alex@tomlins.org.uk>
2017-01-14 18:12:20 +0000

    Support multiple sensors.
    Update the config to provide an update URL for each sensor.

--- config_example.py
+++ config_example.py
@@ -5,4 +5,7 @@ WIFI_NAME='My WiFi Network'
 WIFI_PASSWORD='secret'
 
 W1_GPIO=4
-SENSOR_URL="http://temperature-monitor.example.com:8081/sensors/one"
+
+SENSORS={
+        "12345678": "http://temperature-monitor.example.com:8081/sensors/one",
+        }
--- readtemps.py
+++ readtemps.py
@@ -3,22 +3,34 @@ import urequests
 
 import machine, onewire, ds18x20, time
 
+def load_ds():
+    return ds18x20.DS18X20(onewire.OneWire(machine.Pin(config.W1_GPIO)))
+
+def rom_id(raw):
+    return ''.join("%02x" % b for b in raw)
+
 def run():
-    ds = ds18x20.DS18X20(onewire.OneWire(machine.Pin(config.W1_GPIO)))
+    ds = load_ds()
     roms = ds.scan()
 
     while True:
         print("Reading temperature")
         ds.convert_temp()
         time.sleep_ms(750)
-        temp = ds.read_temp(roms[0])
-        print("Read temperature %f" % temp)
-        data = {"temperature": int(temp * 1000)}
-        resp = urequests.put(config.SENSOR_URL, json=data)
-        if resp.status_code != 200:
-            print("Non-200 response %d" % resp.status_code)
-            print(resp.reason)
-            print(resp.text)
+        for rom in roms:
+            r = rom_id(rom)
+            if r in config.SENSORS:
+                url = config.SENSORS[r]
+                temp = ds.read_temp(rom)
+                print("Sensor %s, Read temperature %f" % (r, temp))
+                data = {"temperature": int(temp * 1000)}
+                resp = urequests.put(url, json=data)
+                if resp.status_code != 200:
+                    print("Sensor %s, Non-200 response %d" % (r, resp.status_code))
+                    print(resp.reason)
+                    print(resp.text)
+            else:
+                print("Unknown rom_id %s" % r)
 
         print("Sleeping for 60 secs")
         time.sleep(60)
