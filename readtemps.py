4dcafcfee02d80e31bcffd9907315b0a48cdeae7
Alex Tomlins <alex@tomlins.org.uk>
2017-03-12 17:09:38 +0000

    DRY readtemps by extracting read_temps function
    This removes the duplicaton between run and read_and_sleep. It also
    means that the circuit can cope with sensors being added/removed.

--- readtemps.py
+++ readtemps.py
@@ -9,37 +9,10 @@ def load_ds():
 def rom_id(raw):
     return ''.join("%02x" % b for b in raw)
 
-def run():
+def read_temps():
     ds = load_ds()
     roms = ds.scan()
-
-    while True:
-        print("Reading temperature")
-        ds.convert_temp()
-        time.sleep_ms(750)
-        for rom in roms:
-            r = rom_id(rom)
-            temp = ds.read_temp(rom)
-            print("Sensor %s, Read temperature %f" % (r, temp))
-            if r in config.SENSORS:
-                url = config.SENSORS[r]
-                data = {"temperature": int(temp * 1000)}
-                resp = urequests.put(url, json=data)
-                if resp.status_code != 200:
-                    print("Sensor %s, Non-200 response %d" % (r, resp.status_code))
-                    print(resp.reason)
-                    print(resp.text)
-            else:
-                print("No config for rom_id %s" % r)
-
-        print("Sleeping for 60 secs")
-        time.sleep(60)
-
-def read_and_sleep():
-    ds = load_ds()
-    roms = ds.scan()
-
-    print("Reading temperature")
+    print("Reading temperatures")
     ds.convert_temp()
     time.sleep_ms(750)
     for rom in roms:
@@ -56,6 +29,16 @@ def read_and_sleep():
                 print(resp.text)
         else:
             print("No config for rom_id %s" % r)
+
+def run():
+    while True:
+        read_temps()
+
+        print("Sleeping for 60 secs")
+        time.sleep(60)
+
+def read_and_sleep():
+    read_temps()
 
     print("Deep sleeping for 60 secs")
     rtc = machine.RTC()
