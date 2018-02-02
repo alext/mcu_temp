35389dc6e5fef15b563fc7c7334952891fef0a1b
Alex Tomlins <alex@tomlins.org.uk>
2018-02-02 11:21:33 +0000

    Extract wifi setup into separate module.
    Simplifies code in main which will make future refactoring easier.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import machine, network, sys, time
 
 import config
+import wifi
 import readtemps
 
 def deep_sleep(duration):
@@ -10,30 +11,10 @@ def deep_sleep(duration):
     rtc.alarm(rtc.ALARM0, duration * 1000)
     machine.deepsleep()
 
-def wifi_connect(nic):
-    if nic.isconnected():
-        print('already connected to network, config:', nic.ifconfig())
-        return
-
-    print('connecting to network...')
-    nic.active(True)
-    nic.connect(config.WIFI_NAME, config.WIFI_PASSWORD)
-    print('waiting for connection')
-    while not nic.isconnected():
-        time.sleep_ms(100)
-        print('.')
-    print('network config:', nic.ifconfig())
-
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
     print('woke from a deep sleep')
 
-ap = network.WLAN(network.AP_IF)
-if ap.active():
-    print('Disabling AP_IF')
-    ap.active(False)
-
-nic = network.WLAN(network.STA_IF)
-wifi_connect(nic)
+wifi.setup()
 
 try:
     readtemps.read_temps()
---
+++ wifi.py
@@ -0,0 +1,26 @@
+import network, time
+
+import config
+
+def connect(nic):
+    if nic.isconnected():
+        print('already connected to network, config:', nic.ifconfig())
+        return
+
+    print('connecting to network...')
+    nic.active(True)
+    nic.connect(config.WIFI_NAME, config.WIFI_PASSWORD)
+    print('waiting for connection')
+    while not nic.isconnected():
+        time.sleep_ms(100)
+        print('.')
+    print('network config:', nic.ifconfig())
+
+def setup():
+    ap = network.WLAN(network.AP_IF)
+    if ap.active():
+        print('Disabling AP_IF')
+        ap.active(False)
+
+    sta = network.WLAN(network.STA_IF)
+    connect(sta)
