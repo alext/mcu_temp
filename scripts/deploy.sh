cedbc40e4c76c9b78ae278dcbdd4c0ba1a57fc07
Alex Tomlins <alex@tomlins.org.uk>
2019-07-06 10:45:50 +0100

    Add script to upload all the code to a device

---
+++ scripts/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -eu
+
+PORT=$1
+BAUD=${2:-115200}
+
+ampy --port "${PORT}" --baud "${BAUD}" put vendor/urequests.py
+ampy --port "${PORT}" --baud "${BAUD}" put boot.py
+ampy --port "${PORT}" --baud "${BAUD}" put readtemps.py
+ampy --port "${PORT}" --baud "${BAUD}" put wifi.py
+ampy --port "${PORT}" --baud "${BAUD}" put config.py
+ampy --port "${PORT}" --baud "${BAUD}" put main.py
